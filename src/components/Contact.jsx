4d758ce838734f5137754bba17c25d13c8f4009d
paramesh-reddy <gurakaparameshwerreddy@gmail.com>
2024-10-21 23:24:17 +0530

    changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Sainathreddy Guraka",
+          to_name: "ParameshWerReddy Guraka",
           from_email: form.email,
-          to_email: "sainathreddyguraka@gmail.com",
+          to_email: "parameshreddy22932@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
